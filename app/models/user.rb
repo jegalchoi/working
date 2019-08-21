e00206b5c712799ea7f2e7151491f1aa0bc0cab0
jegalchoi <thefreshbegal@gmail.com>
2019-08-21 14:08:15 -0700

    Create users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,18 +8,18 @@ class User < ApplicationRecord
 
   attr_reader :password
 
-  def find_by_credentials(username, password)
+  def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
     return user if !user.nil? && user.is_password?(password)
     nil
   end
 
-  def User.generate_session_token
+  def self.generate_session_token
     SecureRandom::urlsafe_base64(16)
   end
 
   def reset_session_token!
-    self.session_token = self.generate_session_token
+    self.session_token = User.generate_session_token
     self.save!
     self.session_token
   end
@@ -36,6 +36,6 @@ class User < ApplicationRecord
   private
 
   def ensure_session_token
-    self.session_token ||= self.generate_session_token
+    self.session_token ||= User.generate_session_token
   end
 end
